bcee7aa9fbc4a262dc55aff6a3cd0c770a7d0055
Kristoffer Dalby <kradalby@kradalby.no>
2020-04-15 16:48:55 +0100

    Format service better

--- mdns-repeater.rb
+++ mdns-repeater.rb
@@ -12,22 +12,25 @@ class MdnsRepeater < Formula
   plist_options :manual => "mdns-repeater"
 
   def plist; <<-EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{bin}/mdns-repeater -f lo0 utun2</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-      </dict>
-    </plist>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+  <dict>
+    <key>Label</key>
+    <string>#{plist_name}</string>
+    <key>ProgramArguments</key>
+    <array>
+      <string>#{bin}/mdns-repeater</string>
+      <string>-f</string>
+      <string>lo0</string>
+      <string>utun2</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <true/>
+  </dict>
+</plist>
     EOS
   end
 
